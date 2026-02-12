0b08e7ad28b0cc3c0dd3ed6bed70c65a020d0dc3
Sunny Rajput <sunnyrajput0247@gmail.com>
2026-02-12 16:04:10 +0530

    feat: Final Polish - Shooting impact effects, sound overlap fix, and updated dev manual

--- Assets/Script/GunShot.cs
+++ Assets/Script/GunShot.cs
@@ -2,14 +2,11 @@ using UnityEngine;
 
 public class GunShot : MonoBehaviour
 {
-
     [SerializeField] Transform playerCamera;
+    [SerializeField] AudioSource gunSound;
     public float bulletRange = 100f;
-
-    void Start()
-    {
-
-    }
+    public float damage = 25f;
+    public GameObject hitEffect;
 
     void Update()
     {
@@ -21,12 +18,22 @@ public class GunShot : MonoBehaviour
 
     void Shoot()
     {
+        gunSound.PlayOneShot(gunSound.clip);  
         Ray ray = new Ray(playerCamera.position, playerCamera.forward);
         RaycastHit hit;
 
         if (Physics.Raycast(ray, out hit, bulletRange))
         {
-            Debug.Log("Hit: " + hit.collider.gameObject.name);
+            GameObject impactGO = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 0.5f);
+            
+            //Debug.Log("Hit: " + hit.collider.gameObject.name);
+
+            EnemyHealth enemy = hit.collider.GetComponent<EnemyHealth>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
         }
     }
 }
