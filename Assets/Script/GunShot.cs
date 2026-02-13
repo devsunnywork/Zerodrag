76d25745c1f69bbd706368c5a1e3bf2a459030e0
Sunny Rajput <sunnyrajput0247@gmail.com>
2026-02-13 22:40:27 +0530

    Feature: Implement basic ammo system in GunShot.cs

--- Assets/Script/GunShot.cs
+++ Assets/Script/GunShot.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+using TMPro;
 
 public class GunShot : MonoBehaviour
 {
@@ -7,10 +9,44 @@ public class GunShot : MonoBehaviour
     public float bulletRange = 100f;
     public float damage = 25f;
     public GameObject hitEffect;
+    public float maxAmmo = 10f;
+    public float reloadTime = 2f;
+    [SerializeField] AudioClip reloadSound;
+    [SerializeField] TextMeshProUGUI ammoText;
+    private float currentAmmo;
+    private bool isReloading = false;
+
+
+    void Start()
+    {
+        currentAmmo = maxAmmo;
+        if (this.enabled) UpdateAmmoUI();
+        else if (ammoText != null) ammoText.text = ""; // Hide if not held
+    }   
+
+    void OnEnable()
+    {
+        UpdateAmmoUI();
+    }
+
+    void OnDisable()
+    {
+        if (ammoText != null) ammoText.text = ""; // Hide when dropped/disabled
+    }
 
     void Update()
+
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (isReloading)
+            return;
+
+        if (currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (Input.GetButtonDown("Fire1") || Input.GetMouseButtonDown(0))
         {
             Shoot();
         }
@@ -18,6 +54,9 @@ public class GunShot : MonoBehaviour
 
     void Shoot()
     {
+
+        currentAmmo--;
+        UpdateAmmoUI();
         gunSound.PlayOneShot(gunSound.clip);  
         Ray ray = new Ray(playerCamera.position, playerCamera.forward);
         RaycastHit hit;
@@ -35,5 +74,30 @@ public class GunShot : MonoBehaviour
                 enemy.TakeDamage(damage);
             }
         }
+    }
+     
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading...");
+
+        if (ammoText != null)
+            ammoText.text = "Reloading...";
+
+        if (reloadSound != null)
+            gunSound.PlayOneShot(reloadSound);
+
+        yield return new WaitForSeconds(reloadTime);
+
+        currentAmmo = maxAmmo;
+        isReloading = false;
+        UpdateAmmoUI();
+        Debug.Log("Reload Complete!");
+    }
+
+    void UpdateAmmoUI()
+    {
+        if (ammoText != null)
+            ammoText.text = "Ammo: " + currentAmmo + " / " + maxAmmo;
     }
 }
